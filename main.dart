cc635a39d4e856d8683cdca67aae1b09e09b5329
Khan-Mol <127586558+Khan-Mol@users.noreply.github.com>
2024-10-14 12:42:13 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,44 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/authentication/login_screen.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(MyApp(savedThemeMode: savedThemeMode));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key, required this.savedThemeMode});
+
+  final AdaptiveThemeMode? savedThemeMode;
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveTheme(
+      light: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.deepPurple,
+      ),
+      dark: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.deepPurple,
+      ),
+      initial: savedThemeMode ?? AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Chat Pro',
+        theme: theme,
+        darkTheme: darkTheme,
+        home: const LoginScreen(),
+      ),
+    );
+  }
+}
